34852b29ffae3855c5934fb5e390e9f8d578dffc
Rümeysa <114934233+rumeysakaravuss@users.noreply.github.com>
2023-03-29 16:38:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function app() {
+    return (
+        <>
+            <button>-</button>
+            <span>0</span>
+            <button>+</button>
+        </>
+    )
+}
+
+export default app ;
